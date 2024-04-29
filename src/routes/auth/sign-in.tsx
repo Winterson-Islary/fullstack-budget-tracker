39cf07ac3d16d17089fa5fb233f678f7adde9730
Winterson <u19cse1028@cit.ac.in>
2024-04-30 02:48:28 +0530

    learn better routing

--- src/routes/auth/sign-in.tsx
+++ src/routes/auth/sign-in.tsx
@@ -1,12 +1,8 @@
 import { Config } from "@/lib/config"
 import { SignIn } from "@clerk/clerk-react"
-import { useLocation} from "react-router-dom"
-
 
 function Sign_In() {
-  const location = useLocation();
-  const redirectLocation =  location.state?.from //|| Config.get("after_sign_in");
-  console.log(redirectLocation)
+  const redirectLocation = Config.get("after_sign_in");
 
   return (
     <SignIn signUpUrl={Config.get("sign_up_url")} forceRedirectUrl={redirectLocation} />
