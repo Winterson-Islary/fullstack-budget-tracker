4a892128e79e2a776f810e7701bf6be900b65d66
Winterson <u19cse1028@cit.ac.in>
2024-05-08 19:36:30 +0530

    provide header authorization bearer to authenticate backend requests

--- src/components/CurrencyComboBox.tsx
+++ src/components/CurrencyComboBox.tsx
@@ -15,21 +15,29 @@ import {
 } from "@/components/ui/popover";
 import { useMediaQuery } from "@/hooks/user-media-query";
 import { Currencies, type Currency } from "@/lib/currencies";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { useQuery } from "@tanstack/react-query";
+import { AuthContext } from "./AuthContext";
 
 export function CurrencyComboBox() {
 	const [open, setOpen] = useState(false);
 	const isDesktop = useMediaQuery("(min-width: 768px)");
 	const [selectedOption, setSelectedOption] = useState<Currency | null>(null);
+	const auth = useContext(AuthContext);
 
+	// useEffect(() => {
 	const userSettings = useQuery({
 		queryKey: ["userSettings"],
 		queryFn: async () =>
-			fetch("http://localhost:3000/api/settings").then((res) => res.json()),
+			fetch("http://localhost:3000/api/settings", {
+				headers: {
+					Authorization: `Bearer ${await auth?.getToken()}`,
+				},
+			}).then((res) => res.json()),
 	});
 	console.log("@@@ USER SETTINGS", userSettings);
+	// }, [auth]);
 
 	if (isDesktop) {
 		return (
