0e9ebc613b35f6c3bbf0163cc66c9901c44df7a5
Winterson <u19cse1028@cit.ac.in>
2024-05-13 15:19:25 +0530

    add zod schema validation

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,3 +6,16 @@ export type UserSettings = {
 	userID: string;
 	currency: string;
 };
+
+import { Currencies } from "./currencies";
+import { z } from "zod";
+
+export const UpdateUserSettingsSchema = z.object({
+	currency: z.custom((value) => {
+		const found = Currencies.some((currency) => currency.value === value);
+		if (!found) {
+			throw new Error(`invalid currency: ${value}`);
+		}
+		return value;
+	}),
+});
