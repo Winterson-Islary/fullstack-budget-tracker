20d6a09177b1918664fd175e9defbbffec9a1b88
Winterson <u19cse1028@cit.ac.in>
2024-05-16 00:07:30 +0530

    display user settings on dashboard

--- src/routes/dashboard/dashboard.tsx
+++ src/routes/dashboard/dashboard.tsx
@@ -1,7 +1,22 @@
-import { Link } from "react-router-dom";
+import { AuthContext } from "@/components/AuthContext";
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { Link, Navigate } from "react-router-dom";
 
 export default function Dashboard() {
-	//! Querying userSettings from backend
+	const auth = useContext(AuthContext);
+	const userSettings = useQuery({
+		queryKey: ["useSettings"],
+		queryFn: async () =>
+			fetch("http://localhost:3000/api/settings", {
+				headers: {
+					Authorization: `Bearer ${await auth?.getToken()}`,
+				},
+			}).then((res) => res.json()),
+	});
+	if (!userSettings) {
+		return <Navigate to="/wizard" replace />;
+	}
 	return (
 		<div className="mt-2">
 			<span>
