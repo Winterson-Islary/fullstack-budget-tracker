62a8fb715103d699fbb9608ed5416f3c0ed29f30
Winterson <u19cse1028@cit.ac.in>
2024-05-31 23:43:38 +0530

    added state to store the selected category during transaction creation

--- src/components/CreateTransactionDialog.tsx
+++ src/components/CreateTransactionDialog.tsx
@@ -3,7 +3,7 @@ import {
 	type CreateTransactionSchemaType,
 	type TransactionType,
 } from "@/lib/types";
-import type { ReactNode } from "react";
+import { useCallback, type ReactNode } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
@@ -38,6 +38,14 @@ function CreateTransactionDialog({ trigger, type }: Props) {
 			date: new Date(),
 		},
 	});
+
+	const handleCategoryChange = useCallback(
+		(value: string) => {
+			form.setValue("category", value);
+		},
+		[form],
+	);
+
 	return (
 		<Dialog>
 			<DialogTrigger asChild>{trigger}</DialogTrigger>
@@ -97,7 +105,10 @@ function CreateTransactionDialog({ trigger, type }: Props) {
 								render={(/*{ field }*/) => (
 									<FormItem>
 										<FormLabel className="mr-3">Category</FormLabel>
-										<CategoryPicker type={type} />
+										<CategoryPicker
+											type={type}
+											onChange={handleCategoryChange}
+										/>
 										<FormDescription>
 											Select a category for this transaction
 										</FormDescription>
